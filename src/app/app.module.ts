482be7be6bcb74ec61df4cb0f250e3b0d286704b
cdonea <claudia.donea24@gmail.com>
2017-09-10 12:02:46 +0100

    Memory game challenge in 2h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { RoundPageModule } from './round-page';
+import { GameGridModule } from './components/game-grid';
+
 @NgModule({
     imports: [
         BrowserModule,
+        RoundPageModule,
+        GameGridModule,
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent],
